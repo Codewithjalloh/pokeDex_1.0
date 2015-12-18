655f1b1ae361ce59ee9c70278f01227b9a463e20
macbook pro owner <WealthyJalloh@jallohx-2.local>
2015-12-18 21:18:29 +0000

    Initial Commit

---
+++ pokerDex-codeWithJalloh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokerDex-codeWithJalloh
+//
+//  Created by Jalloh on 18/12/2015.
+//  Copyright © 2015 ThinkingHouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
